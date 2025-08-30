4530fe9d4d55b9903fc92125c88aae8b599afa75
Yousef10 <yousef_espetan@yahoo.com>
2025-08-30 18:20:27 +0300

    The main Load up function with local domain to upload the Core of CodeMirror6

--- HurfEditor/Form1.cs
+++ HurfEditor/Form1.cs
@@ -1,3 +1,5 @@
+using Microsoft.Web.WebView2.Core;
+
 namespace HurfEditor
 {
     public partial class Form1 : Form
@@ -5,6 +7,30 @@ namespace HurfEditor
         public Form1()
         {
             InitializeComponent();
+
+            InitializeWebViewAsync();
+
         }
+
+
+
+
+        private async void InitializeWebViewAsync()
+        {
+            await WebView.EnsureCoreWebView2Async(null);
+            
+
+            string webAppPath = Path.Combine(AppContext.BaseDirectory, "WebApp", "dist");
+
+            WebView.CoreWebView2.SetVirtualHostNameToFolderMapping(
+                "cm-app.local",
+                webAppPath,
+                CoreWebView2HostResourceAccessKind.Allow
+            );
+
+            WebView.CoreWebView2.Navigate("https://cm-app.local/index.html");
+        }
+
+
     }
 }
