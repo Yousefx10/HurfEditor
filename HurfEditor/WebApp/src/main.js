7a7f26ef417c7593ec9f0d0f0b8be4b88b45c7de
Yousef10 <yousef_espetan@yahoo.com>
2025-08-31 17:06:34 +0300

    Adding colorPicker so color variables can be easly changed in the editor

--- HurfEditor/WebApp/src/main.js
+++ HurfEditor/WebApp/src/main.js
@@ -1,26 +1,58 @@
+//Basic and important IMPORTS:
 import { EditorState } from "@codemirror/state";
-import { EditorView, keymap } from "@codemirror/view";
-import { defaultKeymap } from "@codemirror/commands";
+import { EditorView } from "@codemirror/view";
 import { basicSetup } from "codemirror";
+
+//CODE SYNTAX FOR LANGUAGES:
+import { html } from "@codemirror/lang-html";
 import { javascript } from "@codemirror/lang-javascript";
+import { css } from "@codemirror/lang-css";
 
-const initialCode = `// Simple CodeMirror 6 Editor inside WinForms
+//More imports:
+import { colorPicker, wrapperClassName } from "@replit/codemirror-css-color-picker";
 
-function greet(name) {
-  console.log(\`Hello, \${name}!\`);
-}
+//Unused imports, FOR NOW:
+//import { keymap } from "@codemirror/view";
+//import { defaultKeymap } from "@codemirror/commands";
 
-greet("Developer");
+
+
+// dynamically inject styles:
+const style = document.createElement("style");
+style.textContent = `
+  .${wrapperClassName} {}
+  .${wrapperClassName} input[type="color"] {}
+
 `;
+document.head.appendChild(style);
 
-new EditorView({
-  state: EditorState.create({
-    doc: initialCode,
-    extensions: [
-      basicSetup,
-      keymap.of(defaultKeymap),
-      javascript()
-    ]
-  }),
+
+
+
+
+
+const state = EditorState.create({
+  doc: `<!DOCTYPE html>
+  <html>
+  <head>
+    <style>
+      .box { color: #FF00FF; }
+    </style>
+  </head>
+  <body>
+  <script>
+    function greet(name) {
+      console.log("Hello " + name);
+    }
+    greet("Developer");
+  </script>
+  </body>
+  </html>
+  `,
+  extensions: [basicSetup, html(),css(),javascript(), colorPicker]
+});
+
+const view = new EditorView({
+  state,
   parent: document.getElementById("editor"),
 });
